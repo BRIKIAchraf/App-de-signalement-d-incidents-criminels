b3c43b9d463b2db75e2b02920acc48c8178a14ad
ashraf breki <brikiachraf@DJERBA.r-iset.tn>
2025-04-28 14:31:58 +0200

    setup middleware for dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,17 @@
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
     const token = req.nextauth.token;
     const isAdmin = token?.role === "ADMIN";
     const isModerator = token?.role === "MODERATOR";
-
     if (
       req.nextUrl.pathname.startsWith("/dashboard") &&
       !isAdmin &&
       !isModerator
     ) {
-      return NextResponse.redirect(new URL("/auth/signin", req.url));
+      return NextResponse.redirect(new URL("/api/auth/signin", req.url));
     }
   },
   {
